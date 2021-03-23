5ca3df47dcf61571d6951165b93f32f03b5bd91d
sungjaechoi <sungjaechoi39@gmail.com>
2021-03-23 11:08:34 +0900

    초기 커밋

---
+++ 코딩연습/js/const.js
@@ -0,0 +1,23 @@
+//변수 : 먼가 담는 그릇
+// const / let  +  변수명  +   =   담을 내용
+const age1 = 31;
+let age2 = 31;
+const name1 = "조민혁";
+let name2 = "조민혁";
+name2 = "최성재";
+
+let minhyuk = true; //참
+let minhyuk2 = false; //거짓
+
+//진위값 boolean 참 거짓
+//const로 선언한 변수는 변경이 안됨.
+//확인하는 방법
+// console.log()  괄호안에 변수명을 넣어서 확인
+
+let number1 = 20;
+let number2 = 50;
+
+let sum = number1 * number2; //70
+let string = "number1 + number2를 곱하면";
+let result = string + sum;
+console.log(result);
